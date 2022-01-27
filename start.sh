e78ded8f05423c0bcc939a6fcae6ccadf3b26178
Rijul-A <31570722+Rijul-A@users.noreply.github.com>
2022-01-27 17:24:12 +0000

    Mute Spotify when running

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./spotrec.py -o ./spotify --skip-intro -a -ac mp3 --filename-pattern "{artist}/{album}/{artist} - {title}"
+./spotrec.py -o ./spotify --skip-intro -m -a -ac mp3 --filename-pattern "{artist}/{album}/{artist} - {title}"
